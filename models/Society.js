1e1408cb7b016173d843a271487232c9f2a0a198
Dishant106 <dishant.madariya@swiftrut.com>
2024-11-13 01:02:14 +0530

    fixes

--- models/Society.js
+++ models/Society.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const societySchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     address: {
         type: String,
