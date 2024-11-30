f6a78f970b9375f1ff743dcc42dc29760e407977
SrMehul108 <mehul.gupta@swiftrut.com>
2024-12-01 00:01:04 +0530

    Lazy Load

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -2,3 +2,4 @@ export { Dashboard as AdminDashBoard } from "./Dashboard/";
 export { Announcement } from "./Announcement/";
 export { FacilityManagement  } from "./FacilityManagement/";
 export {ResidentManagement} from "./ResidentManagement"
+export * from "./FinancialMaintenance/Note"
