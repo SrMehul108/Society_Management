5646577cf5493c96bcef8f804e31295044d64966
SrMehul108 <mehul.gupta@swiftrut.com>
2024-12-04 02:49:41 +0530

    fixes

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ app.use(Passport.initialize());
 app.use(Passport.session());
 app.use('/', require('./routes/index'));
 
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
