89e5b20b21cbce1194315844bc1b4f3fbd4bc16d
SrMehul108 <mehul.gupta@swiftrut.com>
2024-12-05 22:48:01 +0530

    Fix Design

--- src/components/SecurityManagement/Protocols/SecurityView.jsx
+++ src/components/SecurityManagement/Protocols/SecurityView.jsx
@@ -1,35 +1,40 @@
-import React from 'react';
+import React from "react";
+import { Icons } from "../../../constants";
 
 function SecurityView({ data, closeModal }) {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-white p-6 rounded-lg max-w-md w-full shadow-lg">
-        <div className="flex justify-between items-start mb-4 ">
+        <div className="flex justify-between items-start">
           <h2 className="text-xl font-semibold">View Security Protocol </h2>
-          <button onClick={closeModal} className="text-gray-500 hover:text-gray-700  text-xl ">&times;</button>
+          <button
+            onClick={closeModal}
+            className="text-gray-500  hover:text-gray-700"
+          >
+            <span className="text-lg">{Icons.Close}</span>
+          </button>
         </div>
 
-        
+        <hr className="mt-6" />
 
-        <div className="mb-4">
-          <p className="text-sm font-medium text-gray-700">Title</p>
+        <div className="mt-6">
+          <p className="text-sm font-medium text-gray-400">Title</p>
           <p className="text-base">{data.title}</p>
         </div>
-        <div className="mb-4">
-          <p className="text-sm font-medium text-gray-700">Description</p>
+        <div className="mt-6">
+          <p className="text-sm font-medium text-gray-400">Description</p>
           <p className="text-base">{data.description}</p>
         </div>
 
-        <div className="grid grid-cols-4 gap-2 text-center">
+        <div className="grid grid-cols-4 gap-2 mt-6">
           <div>
-            <p className="text-sm font-medium text-gray-700">Date</p>
-            <p className="text-base font-medium text-blue-600">{data.date}</p>
+            <p className="text-sm font-medium text-gray-400">Date</p>
+            <p className="text-base font-medium text-gray-700">{data.date}</p>
           </div>
           <div>
-            <p className="text-sm font-medium text-gray-700">Time</p>
-            <p className="text-base font-medium text-black-600">{data.time}</p>
+            <p className="text-sm font-medium text-gray-400">Time</p>
+            <p className="text-sm font-medium text-gray-700">{data.time}</p>
           </div>
-         
         </div>
       </div>
     </div>
