686cd619501da9d629fa7692fca056d8ccb65912
SrMehul108 <mehul.gupta@swiftrut.com>
2024-12-07 23:05:31 +0530

    socket IO Notification

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,12 +1,9 @@
-// Notification.js
-import React, { useState, useEffect } from 'react';
+import { useEffect } from "react";
 
 const Notification = ({ isVisible, message, type, onClose }) => {
   useEffect(() => {
     if (isVisible) {
-      const timer = setTimeout(() => {
-        onClose();
-      }, 3000); // Hide after 3 seconds
+      const timer = setTimeout(onClose, 3000); // Auto-hide after 3 seconds
       return () => clearTimeout(timer);
     }
   }, [isVisible, onClose]);
@@ -14,11 +11,13 @@ const Notification = ({ isVisible, message, type, onClose }) => {
   if (!isVisible) return null;
 
   return (
-    <div className={`fixed  right-36 top-[6%] p-4 rounded-md shadow-lg w-96 h-96 z-50 
-    ${type === 'success' ? 'bg-slate-100 text-black' : 'bg-red-500 text-black'}`}>
+    <div
+      className={`fixed right-10 top-10 p-4 rounded-md shadow-lg z-50 
+      ${type === 'success' ? 'bg-green-500 text-white' : 'bg-red-500 text-white'}`}
+    >
       <div className="flex items-center justify-between">
         <span>{message}</span>
-        <button onClick={onClose} className="ml-4 text-black font-bold focus:outline-none">
+        <button onClick={onClose} className="ml-4 text-white font-bold">
           &times;
         </button>
       </div>
