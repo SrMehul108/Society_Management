a048148eeb9eb1ffdcf0b8cca2b62a6103c52acc
SrMehul108 <mehul.gupta@swiftrut.com>
2024-12-07 05:50:08 +0530

    fd

--- src/components/Resident_management/EditStatus_modal.jsx
+++ src/components/Resident_management/EditStatus_modal.jsx
@@ -18,7 +18,7 @@ export const EditStatusModal = ({ onClose }) => {
   };
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-5 z-50">
       <div className="bg-white rounded-lg p-6 shadow-lg w-96">
         <h2 className="text-lg font-semibold mb-4">Residence Status</h2>
 
