62fd4a6ec834469e9c0d945c0a6b09179e9d928a
Dishant106 <dishant.madariya@swiftrut.com>
2024-12-08 17:18:13 +0530

    Added new routes /chat and /call

--- routes/society.js
+++ routes/society.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const { insertSociety, getSociety } = require('../controllers/societyController');
 router.post('/insertSociety', insertSociety);
 router.get('/getSociety', getSociety);
+router.use('/chat', require('./chat'));
+router.use('/call', require('./call'));
 module.exports = router;
