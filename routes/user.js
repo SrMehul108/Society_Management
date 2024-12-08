cda5ff90c699b28f2afef7adcae6a51d9e6a3e01
Dishant106 <dishant.madariya@swiftrut.com>
2024-12-09 00:46:25 +0530

    fixes

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { userDetail } = require('../controllers/residentcontroller');
+const { SocietyFinanceDetail, PendingMaintenanceList } = require('../controllers/authcontroller');
 router.get('/userdetail',userDetail);
-
+router.get('/SocietyBalance', SocietyFinanceDetail);
+router.get('/pendingMaintenanceList', PendingMaintenanceList);
 module.exports = router;
